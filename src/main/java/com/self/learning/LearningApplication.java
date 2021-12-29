b2c06c853da45c3a7cbd72fbff8fc49ac16ba142
code-manish <code.manish94@gmail.com>
2021-12-29 21:14:12 +0530

    profiles

--- src/main/java/com/self/learning/LearningApplication.java
+++ src/main/java/com/self/learning/LearningApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LearningApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(LearningApplication.class, args);
+  }
 }
